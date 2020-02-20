1eea04be672c444bb1c50cce2df2ea5d7ed11213
toantrugen <toantrugen@gmail.com>
2020-02-20 17:05:43 +0700

    Added a list recipe

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -2,8 +2,8 @@ from django.shortcuts import render
 from rest_framework import viewsets, mixins
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.permissions import IsAuthenticated
-from recipe.serializers import TagSerializer, IngredientSerializer
-from core.models import Tag,Ingredient
+from recipe.serializers import TagSerializer, IngredientSerializer, RecipeSerializer
+from core.models import Tag,Ingredient, Recipe
 
 
 
@@ -36,3 +36,14 @@ class IngredientViewSet(BaseRecipeAttrViewSet):
 
     queryset = Ingredient.objects.all()
     serializer_class = IngredientSerializer
+
+class RecipeViewSet(viewsets.ModelViewSet):
+    """Manage recipes in the databases"""
+    serializer_class = RecipeSerializer
+    queryset = Recipe.objects.all()
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = (IsAuthenticated,)
+
+    def get_queryset(self):
+        """Retrieve the recipes for the authenticated user"""
+        return self.queryset.filter(user=self.request.user)
