6470dea29a5e429b45690b5628d75ec0e274dd96
toantrugen <toantrugen@gmail.com>
2020-02-20 16:18:55 +0700

    Add ingredients endpoint

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -2,8 +2,8 @@ from django.shortcuts import render
 from rest_framework import viewsets, mixins
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.permissions import IsAuthenticated
-from recipe.serializers import TagSerializer
-from core.models import Tag
+from recipe.serializers import TagSerializer, IngredientSerializer
+from core.models import Tag,Ingredient
 
 
 
@@ -24,4 +24,21 @@ class TagViewSet(viewsets.GenericViewSet,
     def perform_create(self, serializer):
         """Create a new tag"""
         serializer.save(user=self.request.user)
-        
+
+
+class IngredientViewSet(viewsets.GenericViewSet,
+                        mixins.ListModelMixin):
+
+    """Manager ingredients in the database"""
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = (IsAuthenticated,)
+    queryset = Ingredient.objects.all()
+    serializer_class = IngredientSerializer
+
+    def get_queryset(self):
+        """Return objects for the current authenticated user only"""
+        return self.queryset.filter(user=self.request.user).order_by('-name')
+
+    def perform_create(serlf, serializer):
+        """Create a new ingredient"""
+        serializer.save(user=self.request.user)
